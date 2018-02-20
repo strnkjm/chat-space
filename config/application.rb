75028940c93e0f2695a11c045215365062d23be4
nkjmstr <sentob68@gmail.com>
2018-02-20 17:05:15 +0900

    reshape_application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
-      g.javascripts false
-      g.helper false
-      g.test_framework false
+      g.javascripts      false
+      g.helper           false
+      g.test_framework   false
     end
   end
 end
