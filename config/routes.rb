86073227b3690aecc8a6b6d65a18a66bab50f6aa
nkjmstr <sentob68@gmail.com>
2018-02-21 16:56:25 +0900

    edit_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'messages#index'
-  resource :messages, only: [:index]
+  resource :messages, only: [:index] do
+    resource :groups, only: [:new, :edit, :update]
+  end
   resource :users, only: [:edit, :update]
-  resource :groups, only: [:new, :edit, :update]
 end
