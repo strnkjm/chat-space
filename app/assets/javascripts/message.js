16d244ee291654e0bb787a9d4958f4f5261c4ae3
nkjmstr <sentob68@gmail.com>
2018-03-20 12:17:01 +0900

    use_turbolinks_gem

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   function buildHTML(message){
     var text = '<ul class="message-list">' +
                   '<li class="message">' +
